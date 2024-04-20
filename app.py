bc393b9d77167ead203054f890c9672b24e59a23
Rogelio <rouge@Rogelios-MacBook-Air.local>
2024-04-19 21:19:45 -0500

    added from code, postgresql-psycopg2

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import psycopg2
 from helpers import apology
 from collections import defaultdict
 
