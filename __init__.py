f3bf32e71e7bb567b65f9ae8afd68a3ea83ffd23
Rogelio <rouge@Rogelios-MacBook-Air.local>
2024-04-28 10:24:42 -0500

    new non needed

---
+++ __init__.py
@@ -0,0 +1,76 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Configure application
+app = Flask(__name__)
+
+def create_app():
+    # Configure session to use filesystem (instead of signed cookies)
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
+
+
+    # Configure database
+    #{sgmm+VzG7VE@127.0.0.1:3306/fleet_db (GODADDY CODE)
+    #mysql+pymysql://mydb_root_user@localhost/fleet_db
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://u2uao60uo5rh2g:p67321ffebd10efb688c69c9231e5a4839c03d0e305f2d0a231391dc037f714eb@cb6h87c9erodfl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/damd6vshgk9tdd'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Initialize SQLAlchemy with the app
+    db.init_app(app)
+
+    return app
+
+# Create database object
+db = SQLAlchemy(app)
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
+
+# Define the Entry model used for inbox
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    van = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    
+    # Define a function to get the default timestamp value in CST
+    @staticmethod
+    def default_cst_timestamp():
+        # Get the current UTC time
+        utc_now = datetime.utcnow()
+        # Convert UTC time to CST
+        cst_time = convert_utc_to_cst(utc_now)
+        return cst_time
+
+    # Define the timestamp column with the default value as CST time
+    timestamp = db.Column(db.DateTime, nullable=False, default=default_cst_timestamp)
+
+
+
+#Defines Note used in Admin page to send post it notes between management
+class Note(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+# Define a function to convert UTC to CST
+def convert_utc_to_cst(utc_time):
+    # Calculate the time difference between UTC and CST (UTC-6 hours)
+    cst_offset = timedelta(hours=-6)
+    # Add the offset to the UTC time
+    cst_time = utc_time + cst_offset
+    return cst_time
+
+# Create tables
+with app.app_context():
+    db.create_all()
+
+
+
+# Import routes after creating the app and db
+from . import routes
