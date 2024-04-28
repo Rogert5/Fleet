fe2c39f776447ca0639e4e90114647a983110513
Rogelio <rouge@Rogelios-MacBook-Air.local>
2024-04-28 11:25:08 -0500

    reverted all code to app.py

--- app.py
+++ app.py
@@ -31,7 +31,70 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Configure database
+#{sgmm+VzG7VE@127.0.0.1:3306/fleet_db (GODADDY CODE)
+#mysql+pymysql://mydb_root_user@localhost/fleet_db
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://u2uao60uo5rh2g:p67321ffebd10efb688c69c9231e5a4839c03d0e305f2d0a231391dc037f714eb@cb6h87c9erodfl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/damd6vshgk9tdd'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Create database object
+db = SQLAlchemy(app)
+
+
+# Define the Entry model used for inbox
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    van = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    
+    # Define a function to get the default timestamp value in CST
+    @staticmethod
+    def default_cst_timestamp():
+        # Get the current UTC time
+        utc_now = datetime.utcnow()
+        # Convert UTC time to CST
+        cst_time = convert_utc_to_cst(utc_now)
+        return cst_time
+
+    # Define the timestamp column with the default value as CST time
+    timestamp = db.Column(db.DateTime, nullable=False, default=default_cst_timestamp)
+
+
+
+#Defines Note used in Admin page to send post it notes between management
+class Note(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+# Define a function to convert UTC to CST
+def convert_utc_to_cst(utc_time):
+    # Calculate the time difference between UTC and CST (UTC-6 hours)
+    cst_offset = timedelta(hours=-6)
+    # Add the offset to the UTC time
+    cst_time = utc_time + cst_offset
+    return cst_time
+
+# Create tables
+with app.app_context():
+    db.create_all()
+
+#Delete entry from inbox
+@app.route("/inbox/delete-entry", methods=["POST"])
+def delete_entry():
+    page = request.form.get("page")
+    entry_id = request.form.get("entryId")
+    if entry_id:
+        entry = Entry.query.get(entry_id)
+        if entry:
+            db.session.delete(entry)
+            db.session.commit()
+
+    flash("Entry Deleted",)
+    return redirect("/inbox")
+
+# home page route
 @app.route("/")
 def home():
     return render_template("home.html")
