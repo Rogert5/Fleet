8c608dccf241805c439ea93a769cd7aab3fdbced
Rogelio <rouge@Rogelios-MacBook-Air.local>
2024-05-14 20:51:58 -0500

    databas code for local

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def after_request(response):
 #postgresql://u2uao60uo5rh2g:p67321ffebd10efb688c69c9231e5a4839c03d0e305f2d0a231391dc037f714eb@cb6h87c9erodfl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/damd6vshgk9tdd
 #{sgmm+VzG7VE@127.0.0.1:3306/fleet_db (GODADDY CODE)
 #mysql+pymysql://mydb_root_user@localhost/fleet_db (LOCAL CODE)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://u2uao60uo5rh2g:p67321ffebd10efb688c69c9231e5a4839c03d0e305f2d0a231391dc037f714eb@cb6h87c9erodfl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/damd6vshgk9tdd'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://mydb_root_user@localhost/fleet_db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Create database object
