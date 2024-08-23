c38c0c8c6698e975e865f080c26285062ef20bc6
Rogelio <rouge@Rogelios-MacBook-Air.local>
2024-08-23 18:11:18 -0500

    error deleted escape module

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-from flask import Flask, flash, redirect, render_template, request, session, jsonify, escape
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import psycopg2
